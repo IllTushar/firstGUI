a87b41be6b4f4ad8438b6a53a71c7c5f711248bb
IllTushar <gtushar7668@gmail.com>
2023-12-03 16:18:20 +0530

    3.12.23x1

--- main.py
+++ main.py
@@ -7,18 +7,20 @@ root.title("First GUI")
 # 0 is origin  from x-axis and 0 is origin for  y-axis
 root.geometry(f"{height}x{width}+0+0")
 root.iconbitmap(r"C:\Users\gtush\OneDrive\Desktop\GUI\firstGUI\please-give-me-1.ico")
-entry = tk.Entry(root, width=100)
-entry.pack()
+
+text_box = tk.Text(root)
+text_box.pack()
 
 
 def click():
-    text = entry.get()
-    label = tk.Label(root, text=text)
-    label.pack()
+    text1 = "Hi " + text_box.get('1.0', tk.END)
+    label.config(text=str(text1))
 
 
 btn = tk.Button(root, command=click, text="Click")
 btn.pack()
+label = tk.Label(root)
+label.pack()
 
 # mainloop is used for staying the window when its run
 root.mainloop()
