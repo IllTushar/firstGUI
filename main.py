fb7cc156593bfa2588623fc8ab0386a476ab6dff
IllTushar <gtushar7668@gmail.com>
2023-12-04 20:42:13 +0530

    4.12.23x1

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 
 root = tk.Tk()
 height = 500
@@ -7,6 +8,18 @@ root.title("First GUI")
 # 0 is origin  from x-axis and 0 is origin for  y-axis
 root.geometry(f"{height}x{width}+0+0")
 root.iconbitmap(r"C:\Users\gtush\OneDrive\Desktop\GUI\firstGUI\please-give-me-1.ico")
+
+
+def action():
+    print(mycombo.get())
+
+
+mycombo = ttk.Combobox(root, font='arial', state='readonly', justify='center')
+mycombo['value'] = ('Select the Value', "Java", "Python", "HTML")
+mycombo.set(mycombo['value'][0])
+mycombo.pack()
+btn_click = ttk.Button(root, command=action, text="click")
+btn_click.pack()
 
 # mainloop is used for staying the window when its run
 root.mainloop()
